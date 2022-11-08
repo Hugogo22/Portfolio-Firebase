a0d7d2b7142d18cf83d1ce686cf5869cbc50cf40
eynard <hugo.eynard@gmail.com>
2022-11-08 15:31:05 +0100

    Career development text

--- src/components/CareerDevelopment.vue
+++ src/components/CareerDevelopment.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="min-h-screen bg-center bg-cover bg-space">
-    
+  <div class="min-h-screen bg-center bg-cover bg-space flex items-center justify-center">
+    <div class="text-white font-mono text-lg font-bold w-3/5 text-justify p-10 border-2 border-white rounded-md bg-black bg-opacity-30">
+      <p>
+        My current carreer goal is to work in the space industry as an embedded software engineer. 
+        I have been passionate about space for a long time and it's one of the reasons why I chose to study in Toulouse, which is the heart of the space industry in France.
+      </p>
+      <p>
+        I will meet a systems engineer working in the space industry in November to learn more about the different career paths I can chose.
+      </p>
+    </div>
   </div>
 </template>
 
