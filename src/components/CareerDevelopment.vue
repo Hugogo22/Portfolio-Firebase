f7b298389414e5be44b358a4a951177d94b9268b
eynard <hugo.eynard@gmail.com>
2022-11-08 15:37:25 +0100

    career development ajustement responsive

--- src/components/CareerDevelopment.vue
+++ src/components/CareerDevelopment.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-center bg-cover bg-space flex items-center justify-center">
-    <div class="text-white font-mono text-lg font-bold w-3/5 text-justify p-10 border-2 border-white rounded-md bg-black bg-opacity-30">
+    <div class="text-white font-mono text-lg font-bold md:w-3/5 text-justify p-10 md:border-2 border-white rounded-md bg-black bg-opacity-30">
       <p>
         My current carreer goal is to work in the space industry as an embedded software engineer. 
         I have been passionate about space for a long time and it's one of the reasons why I chose to study in Toulouse, which is the heart of the space industry in France.
